a6a7af75e1163a7c6d04dc3b99472338b3e6499e
wangqian0609 <954919352@qq.com>
2018-07-03 17:48:33 +0800

    增加表情
    增加表情

--- src/components/wxChat.vue
+++ src/components/wxChat.vue
@@ -44,6 +44,11 @@
                     </ul>
                 </div>
 			</ScrollLoader>
+            <div class="emotion">  
+                <ul>
+                    <li v-for="list in lists" v-emotion="'[' + list + ']'" @click="inputEmotion">{{list}}</li>
+                </ul>
+            </div>
 			<div class="sendBox">
 				<input type="text" class="input_word" v-model="NewMessageText">
                 <button class="emotion_Btn">
@@ -245,6 +250,21 @@
         font-size: 15px;
         text-align: center;
     }
+    .emotion{
+        width:100%;
+        position: absolute;
+        left:0;
+        bottom: 50px;
+        background: #fff;
+        font-size:15px;
+    }
+    .emotion ul{
+        width:100%;
+        display: inline-block;
+    } 
+    .emotion ul li {
+        display: inline-block;
+    }
     @keyframes moveRight{
         0%{left:-20px; opacity: 0};
         100%{left:0; opacity: 1}
@@ -269,6 +289,8 @@
 </style>
 <script>
 	import ScrollLoader from './scrollLoader.vue'
+
+    var lists = ['微笑', '撇嘴', '色', '发呆', '得意', '流泪', '害羞', '闭嘴', '睡', '大哭', '尴尬', '发怒', '调皮', '呲牙', '惊讶', '难过', '酷', '冷汗', '抓狂', '吐', '偷笑', '愉快', '白眼', '傲慢', '饥饿', '困', '惊恐', '流汗', '憨笑', '大兵', '奋斗', '咒骂', '疑问', '嘘', '晕', '折磨', '衰', '骷髅', '敲打', '再见', '擦汗', '抠鼻', '鼓掌', '糗大了', '坏笑', '左哼哼', '右哼哼', '哈欠', '鄙视', '委屈', '快哭了', '阴险', '亲亲', '吓', '可怜', '菜刀', '西瓜', '啤酒', '篮球', '乒乓', '咖啡', '饭', '猪头', '玫瑰', '凋谢', '示爱', '爱心', '心碎', '蛋糕', '闪电', '炸弹', '刀', '足球', '瓢虫', '便便', '月亮', '太阳', '礼物', '拥抱', '强', '弱', '握手', '胜利', '抱拳', '勾引', '拳头', '差劲', '爱你', 'NO', 'OK', '爱情', '飞吻', '跳跳', '发抖', '怄火', '转圈', '磕头', '回头', '跳绳', '挥手', '激动', '街舞', '献吻', '左太极', '右太极', '嘿哈', '捂脸', '奸笑', '机智', '皱眉', '耶', '红包', '鸡'];
 
 	export default{
 		name:"wxChat",
@@ -323,7 +345,8 @@
 				maxHeight:700,
 				dataArray:[],
                 nextMessageId:12,
-                NewMessageText:''
+                NewMessageText:'',
+                lists,
 			}
 		},
 		created(){
@@ -412,6 +435,9 @@
                 var Content = document.getElementById('messageList');
                 var Number = Content.clientHeight - this.minHeight + 142;
                 Content.style.transform = "translateY(-" + Number + "px)";
+            },
+            inputEmotion(){
+                this.NewMessageText += this;
             }
 		}
 	}
