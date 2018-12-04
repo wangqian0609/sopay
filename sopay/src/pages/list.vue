fb8211cc4b2ef59a0cae12e2710d3d142aa2b745
wangqian0609 <954919352@qq.com>
2018-12-04 18:06:24 +0800

    备份代码

--- sopay/src/pages/list.vue
+++ sopay/src/pages/list.vue
@@ -2,17 +2,99 @@
 	<div class="sortContent">
 		<nav-header :title="navTitle"></nav-header>
 		<div class="listContent">
-			<div>{{navTitle}}</div>
-			<ul>
+			<div class="listTop" v-bind:style="{backgroundImage:'url(' + salesImg + ')', backgroundRepeat:'no-repeat', backgroundPosition:'center center', backgroundSize: '100% 100%'}">
+				<div class="listBox">
+					<h2>{{navTitle}}</h2>
+					<label>GET UP TO 60% OFF</label>
+				</div>
+			</div>
+			<ul class="listUl">
 				<li v-for="item in lists">
-					{{item.name}}
+					<label>{{item.name}}</label>
+					<div class="item-right">
+						<img :src="item.img">
+					</div>
 				</li>
 			</ul>
 		</div>
 	</div>
 </template>
-<style type="text/css" lang="scss" scoped="">
-	
+<style type="text/css" lang="scss">
+	@import '../common/fonts/font.css';
+
+	.sortContent{
+		width:100%;
+		height:100%;
+		.listContent{
+			width:100%;
+			padding-top:.44rem;
+			.listTop{
+				width:100%;
+				height:1.67rem;
+				text-align:center;
+				background-color:#d8d8d8;
+				display:-webkit-box;
+				-webkit-box-align:center;
+				-webkit-box-pack:center;
+				.listBox{
+					width:2.3rem;
+					// height:1.01rem;
+					display:inline-block;
+					border:1px solid #fff;
+					h2{
+						width:100%;
+						display:inline-block;
+						font-size:.5rem;
+						color:#fff;	
+					}
+					label{
+						width:100%;
+						display:inline-block;
+						padding: .05rem 0;
+						font-size:.14rem;
+						color:#fff;
+						border-top:1px solid #fff;
+					}
+				}
+			}
+			.listUl{
+				width:100%;
+				li{
+					width:100%;
+					height:1.11rem;
+					padding:0 .22rem 0 .5rem;
+					position:relative;
+					label{
+						line-height:1.11rem;
+						display:inline-block;
+						font-family:'Montserrat';
+						font-size:.16rem;
+						color:#767777;
+					}
+					.item-right{
+						width:2rem;
+						height:100%;
+						display:-webkit-box;
+						-webkit-box-align:center;
+						-webkit-box-pack:center;
+						overflow:hidden;
+						position:absolute;
+						right:.2rem;
+						top:0;
+						img{
+							height:100%;
+						}
+					}
+					&:nth-child(2n+1){
+						background-color:#e6e6e6;
+					}
+					&:nth-child(2n){
+						background-color:#D1C1AF;
+					}
+				}
+			}
+		}
+	}
 </style>
 <script type="text/javascript">
 	import navHeader from '../components/Hheader'
